79903ca35379c02ecba1a9349ef86dc78a535eeb
Diogo Vieira <60749477+xXD4rkSoulXx@users.noreply.github.com>
2024-05-31 18:05:02 +0100

    Update pagina_home.dart
    Depois altero o resto que falta, é apenas pra já ficar feito o que eu já mudei

--- lib/paginas/pagina_home.dart
+++ lib/paginas/pagina_home.dart
@@ -1,6 +1,4 @@
 import "package:flutter/material.dart";
-//import ""; //Página Login
-//import ""; //Página Mensagens
 
 void main() {
   runApp(Pagina_Home());
@@ -14,23 +12,7 @@ class Pagina_Home extends StatefulWidget {
 }
 
 class Pagina_HomeEstado extends State<Pagina_Home> {
-  int quantidade_utilizadores = 12;
-  //A quantidade de nomes inseridos dentro desta lista tem de estar de acordo com a variável quantidade_utilizadores
-  //Esta lista de utilizadores são exemplos, que vai ser aqui depois adicionar o código para ser lista de pedidos de amizades da base de dados
-  final List<String> utilizadores = [
-    "antonuolink",
-    "ToacyVaiMeDar20",
-    "MrEstudador",
-    "Afonso295",
-    "Cellbit",
-    "SigmaMale",
-    "5ugg4rM0mmy",
-    "AmanteN21DoAntonio",
-    "Bill_Gates",
-    "iShowSpeed",
-    "_Obama_",
-    "JohanLiebert2817"
-  ];
+  int quantidade_pedidos = 12;
 
   @override
   Widget build(BuildContext context) {
@@ -39,12 +21,10 @@ class Pagina_HomeEstado extends State<Pagina_Home> {
         home: Scaffold(
             body: Row(children: [
           Expanded(
-              //O código dentro deste Expanded é a parte esquerda
               flex: 1,
               child: Column(children: [
-                if (quantidade_utilizadores == 0)
+                if (quantidade_pedidos == 0)
                   Expanded(
-                      //O código dentro deste Expanded é caso não haja pedidos de amizades
                       child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
@@ -52,53 +32,32 @@ class Pagina_HomeEstado extends State<Pagina_Home> {
                       ]))
                 else
                   Expanded(
-                      //O código dentro deste Expanded é a parte do código em que aparece a lista de pedidos de amizade
                       child: ListView.builder(
-                          itemCount: quantidade_utilizadores,
+                          itemCount: quantidade_pedidos,
                           itemBuilder: (context, i) {
                             return Column(children: [
                               Container(
                                   color: Colors.grey[200],
                                   padding: EdgeInsets.symmetric(vertical: 8.0),
                                   child: ListTile(
-                                      title: Text(utilizadores[i]),
+                                      title: Text(""),
                                       trailing: Row(
                                           mainAxisSize: MainAxisSize.min,
                                           children: [
                                             IconButton(
                                                 icon: Icon(Icons.check,
                                                     color: Colors.green),
-                                                onPressed: () {
-                                                  setState(() {
-                                                    /*
-                                                                                        Código de adicionar amigo na Base de dados
-                                                                                        E remover pedido de amizades na Base de Dados
-                                                                                    */
-                                                    //Remover pedido de amizade da tela
-                                                    quantidade_utilizadores--;
-                                                    utilizadores.removeAt(i);
-                                                  });
-                                                }),
+                                                onPressed: () {}),
                                             IconButton(
                                                 icon: Icon(Icons.close,
                                                     color: Colors.red),
-                                                onPressed: () {
-                                                  setState(() {
-                                                    /*
-                                                                                        Código de remover pedido de amizades na Base de Dados
-                                                                                    */
-                                                    //Remover pedido de amizade da tela
-                                                    quantidade_utilizadores--;
-                                                    utilizadores.removeAt(i);
-                                                  });
-                                                })
+                                                onPressed: () {})
                                           ]))),
                               Divider(height: 1, color: Colors.black)
                             ]);
                           })),
                 Divider(height: 1, color: Colors.black),
                 Padding(
-                    //O código dentro deste Padding são os botões de Mensagens e Pedidos
                     padding:
                         EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
                     child: Row(children: [
@@ -106,14 +65,7 @@ class Pagina_HomeEstado extends State<Pagina_Home> {
                           child: IconButton(
                               iconSize: 50,
                               icon: Icon(Icons.message, color: Colors.black),
-                              onPressed: () {
-                                /* //Depois adicionar link para a página do chat com mensagens
-                                                            Navigator.push(
-                                                                context,
-                                                                MaterialPageRoute(builder: (context)=>Mensagens())
-                                                            );
-                                                            */
-                              })),
+                              onPressed: () {})),
                       Expanded(
                           child: IconButton(
                               iconSize: 50,
@@ -123,7 +75,6 @@ class Pagina_HomeEstado extends State<Pagina_Home> {
               ])),
           VerticalDivider(width: 1, color: Colors.black),
           Expanded(
-              //O código dentro Expanded já é a parte normal do site, com a logo, o Bem-vindo e isso tudo
               flex: 3,
               child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -157,44 +108,12 @@ class Pagina_HomeEstado extends State<Pagina_Home> {
                                                   "Adicionar o teu amigo aqui",
                                               suffixIcon: IconButton(
                                                   icon: Icon(Icons.add_circle),
-                                                  onPressed: () {
-                                                    /*
-                                                                            Código para adicionar pedidos de amizades na base de dados
-                                                                        */
-                                                    //Mensagem de confirmação que o pedido de amizade foi enviado com sucesso
-                                                    /*showDialog(
-                                                                            context: context,
-                                                                            builder: (BuildContext context)
-                                                                            {
-                                                                                return AlertDialog(
-                                                                                    content: Text("Mensagem enviada com sucesso!", style: TextStyle(fontSize: 20)),
-                                                                                    actions: <Widget>[
-                                                                                        TextButton(
-                                                                                            onPressed: ()
-                                                                                            {
-                                                                                                Navigator.of(context).pop();
-                                                                                            },
-                                                                                            child: Text('Fechar', style: TextStyle(fontSize: 20)),
-                                                                                        ),
-                                                                                    ]
-                                                                                );
-                                                                            }
-                                                                        );*/
-                                                  }))))),
+                                                  onPressed: () {}))))),
                               SizedBox(width: 20),
                               IconButton(
                                   iconSize: 40,
                                   icon: Icon(Icons.logout),
-                                  onPressed: () {
-                                    /*
-                                                            Código para terminar sessão
-                                                        */
-                                    //Depois de terminar sessão, redirecionar para a página de Login
-                                    /*Navigator.push(
-                                                            context,
-                                                            MaterialPageRoute(builder: (context)=>Login())
-                                                        );*/
-                                  })
+                                  onPressed: () {})
                             ]))
                   ]))
         ])));
