d1947cc98fc4b96e6518d95cf03e025ae1be43b5
antonuolink <151615232+antonuolink@users.noreply.github.com>
2024-06-04 10:53:32 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ESTG-ES1-Chat-Together/lib/paginas/pagina_login.dart'
 
 void main() {
   runApp(const MyApp());
@@ -11,29 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Gaming Together',
-      home: MyHomePage(title: 'Gaming Together'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: Text(widget.title),
-      ),
-      body: const Center(child: Text("Hello World")),
+      home: PaginaLogin(),
     );
   }
 }
