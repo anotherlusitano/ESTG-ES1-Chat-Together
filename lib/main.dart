b8bdb26b7055b50d311b5ca512c2f35958638cf6
antonuolink <151615232+antonuolink@users.noreply.github.com>
2024-06-04 11:08:07 +0100

    ACABOU A HOME

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Gaming Together',
-      home: const PaginaLogin(),
+      home: PaginaLogin(),
     );
   }
 }
