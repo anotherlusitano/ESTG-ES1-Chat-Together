ca3f11255af9ced8d3b9cd9f7eabd5701c61f50c
antonuolink <151615232+antonuolink@users.noreply.github.com>
2024-06-04 11:00:14 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Gaming Together',
-      home: PaginaLogin(),
+      home: const PaginaLogin(),
     );
   }
 }
