e6d4468371b40ebdcf0682c53ef1acc69f13bcf5
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2024-06-09 13:19:20 +0100

    Adicionar sistema de login

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:gaming_together/paginas/pagina_login.dart';
 
-void main() {
+void main() async {
+  // isto serve para garantir que o firebase é inicializado
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
--- lib/paginas/pagina_login.dart
+++ lib/paginas/pagina_login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:gaming_together/widgets/botao_principal.dart';
 
 class PaginaLogin extends StatefulWidget {
@@ -11,6 +12,48 @@ class _PaginaLoginState extends State<PaginaLogin> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   bool _showPassword = false;
+
+  // vai verificar se existe conta
+  // se não houver vai mandar erro
+  // se algo estiver mal, vai dar erro
+  // enquanto isso vai mostrar um círculo de loading
+  void entrar_na_conta() async {
+    showDialog(
+      context: context,
+      builder: (context) => const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _usernameController.text,
+        password: _passwordController.text,
+      );
+
+      //pop loading circule
+      if (context.mounted) Navigator.pop(context);
+
+      // TODO: por aqui a navegação para a página Home
+      // Navigator.push(
+      //   context,
+      //   MaterialPageRoute(builder: (context) => Home()),
+      // );
+    } on FirebaseAuthException catch (erro) {
+      // sair do circulo de loading
+      Navigator.pop(context);
+
+      mostrar_mensagem_erro(erro.message!);
+    }
+  }
+
+  void mostrar_mensagem_erro(String mensagem) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(mensagem),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -80,11 +123,7 @@ class _PaginaLoginState extends State<PaginaLogin> {
               SizedBox(
                 width: 180, // Ajustando a largura para médio
                 child: BotaoPrincipal(
-                  onTap: () {
-                    String username = _usernameController.text;
-                    String password = _passwordController.text;
-                    print('Username: $username, Password: $password');
-                  },
+                  onTap: entrar_na_conta,
                   text: 'Entrar',
                 ),
               ),
