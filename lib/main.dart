b4c0825d988601cd0d916f4889fdd99fa6c7eadc
Afonso295 <152220916+Afonso295@users.noreply.github.com>
2024-06-15 16:49:12 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gaming Together',
+      title: 'Chat Together',
       home: PaginaLogin(),
     );
   }
